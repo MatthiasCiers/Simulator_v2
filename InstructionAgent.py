9c938fd11a8a8f0502777575609e9220ff4502a1
MatthiasCiers <matthias.ciers@hotmail.com>
2025-03-04 09:53:01 +0100

    implementation of validate, match and settle methods in InstructionAgent.py

--- InstructionAgent.py
+++ InstructionAgent.py
@@ -1,13 +1,15 @@
 from datetime import datetime
+from mesa import Agent
 import TransactionAgent
 import SettlementModel
 import InstitutionAgent
+import DeliveryInstructionAgent
+import ReceiptInstructionAgent
 import Account
-from mesa import Agent
 
 class InstructionAgent (Agent):
     def __init__(self, model: SettlementModel, uniqueID: str, motherID: str, institution: InstitutionAgent, securitiesAccount: Account, cashAccount: Account, securityType: str, amount: float, isChild: bool, status: str, linkcode: str, creation_time: datetime, linkedTransaction: TransactionAgent = None):
-        super.__init__(model)
+        super().__init__(model)
         self.uniqueID = uniqueID
         self.motherID = motherID
         self.institution = institution
@@ -18,7 +20,8 @@ class InstructionAgent (Agent):
         self.isChild = isChild
         self.status = status
         self.linkcode = linkcode
-        self.creation_time = datetime.datetime.now() # track creation time for timeout
+        self.creation_time = datetime.now() # track creation time for timeout
+        self.linkedTransaction = linkedTransaction
 
 #getter methods
     def get_model(self):
@@ -72,3 +75,73 @@ class InstructionAgent (Agent):
     def get_creation_time(self):
         """Returns the creation time of the instruction."""
         return self.creation_time
+
+    def set_status(self, new_status: str):
+        self.status = new_status
+
+    def insert_instruction(self):
+        # TODO: is this just changing state from exists to pending?
+        pass
+
+    def validate(self):
+        if self.status == 'Pending':
+            # TODO: second delay has to be implemented still
+            self.set_status('Validated')
+            # TODO: logging to eventlog
+
+    def match(self):
+    #matches this instruction with the other instruction with same linkcode
+    #creates transactionAgent with the 2 instructions
+
+        if self.status == 'Validated':
+            #find other instruction with same linkcode:
+            other_instruction = None
+            for agent in self.model.schedule.agents:
+                if (
+                    isinstance(agent, InstructionAgent) #checks if it is an InstructionAgent
+                    and agent.uniqueID != self.uniqueID #checks if it is not itself
+                    and agent.linkcode == self.linkcode #checks if linkcodes match
+                    and agent.status == 'Validated' #checks status of other instruction
+                ):
+                    if( #check if both instructions are of opposite type
+                        isinstance(self, DeliveryInstructionAgent) and isinstance(agent, ReceiptInstructionAgent)
+                    ) or (
+                        isinstance(self, ReceiptInstructionAgent) and isinstance(agent, DeliveryInstructionAgent)
+                    ):
+                        other_instruction = agent
+                        break
+
+        #create transaction
+        transaction = TransactionAgent(
+            model = self.model,
+            transactionID = f"{self.uniqueID}_{other_instruction.uniqueID}",
+            deliverer = self if isinstance(self, DeliveryInstructionAgent) else other_instruction,
+            receiver = self if isinstance(self, ReceiptInstructionAgent) else other_instruction,
+            status = "Matched"
+        )
+
+        #link transaction to both instructions:
+        self.linkedTransaction = transaction
+        other_instruction.linkedTransaction = transaction
+
+        #update status in both instructions
+        self.set_status("Matched")
+        other_instruction.set_status("Matched")
+
+        # TODO: logging
+
+    def settle(self):
+    #only to change state. Actual settlement logic is in TransactionAgent
+        if self.status == "Matched":
+            self.set_status('Settled')
+
+    def cancel_timout(self):
+        #method to cancel instruction due to timeout
+        # TODO
+        pass
+
+    def cancel_partial(self):
+        #method to cancel instruction due to partial settlement.
+        #this instruction gets cancelled, but children get created
+        # TODO
+        pass
