30cf3b8a61fcf8fc4f4f3f4f14b23a8c6f0e16f4
MatthiasCiers <matthias.ciers@hotmail.com>
2025-03-07 10:15:21 +0100

    changes mistake for partial settelment amount

--- DeliveryInstructionAgent.py
+++ DeliveryInstructionAgent.py
@@ -25,17 +25,23 @@ class DeliveryInstructionAgent(InstructionAgent):
 
     def createDeliveryChildren(self):
         available_securities = self.securitiesAccount.checkBalance(self.amount, self.securityType)
+
+        #takes the minimum of available securities of deliverer and available cash of seller
+        available_to_settle = min(self.securitiesAccount.checkBalance(self.amount, self.securityType),
+                                  self.linkedTransaction.cashAccount.checkBalance(self.amount, self.securityType)
+                                  )
+
         if available_securities > 0:
             #create delivery children instructions
 
             #instant matching and settlement of first child not yet possible, because receipt_child_1 does not yet exist
             delivery_child_1 = InstructionAgent(self.model, f"{self.uniqueID}_1", self.uniqueID,
                                                 self.institution, self.securitiesAccount, self.cashAccount,
-                                                self.securityType, available_securities, True, "Validated", f"{self.linkcode}_1", datetime.now(), None
+                                                self.securityType, available_to_settle, True, "Validated", f"{self.linkcode}_1", datetime.now(), None
                                                 )
             delivery_child_2 = InstructionAgent(self.model, f"{self.uniqueID}_2", self.uniqueID,
                                                 self.institution, self.securitiesAccount, self.cashAccount,
-                                                self.securityType, self.amount - available_securities, True, "Validated", f"{self.linkcode}_2", datetime.now(), None
+                                                self.securityType, self.amount - available_to_settle, True, "Validated", f"{self.linkcode}_2", datetime.now(), None
                                                 )
             #add child instructions to the model
             self.model.schedule.add(delivery_child_1)
--- ReceiptInstructionAgent.py
+++ ReceiptInstructionAgent.py
@@ -28,18 +28,24 @@ class ReceiptInstructionAgent(InstructionAgent):
 
     def createReceiptChildren(self):
         available_cash = self.cashAccount.checkBalance(self.amount, self.securityType)
+
+        # takes the minimum of available securities of deliverer and available cash of seller
+        available_to_settle = min(self.linkedTransaction.securitiesAccount.checkBalance(self.amount, self.securityType),
+                                  self.cashAccount.checkBalance(self.amount, self.securityType)
+                                  )
+
         if available_cash > 0:
             # create delivery children instructions
 
             # instant matching and settlement of first child not yet possible, because receipt_child_1 does not yet exist
             receipt_child_1 = InstructionAgent(self.model, f"{self.uniqueID}_1", self.uniqueID,
                                                 self.institution, self.securitiesAccount, self.cashAccount,
-                                                self.securityType, available_cash, True, "Validated",
+                                                self.securityType, available_to_settle, True, "Validated",
                                                 f"{self.linkcode}_1", datetime, None
                                                 )
             receipt_child_2 = InstructionAgent(self.model, f"{self.uniqueID}_2", self.uniqueID,
                                                 self.institution, self.securitiesAccount, self.cashAccount,
-                                                self.securityType, self.amount - available_cash, True,
+                                                self.securityType, self.amount - available_to_settle, True,
                                                 "Validated", f"{self.linkcode}_2", datetime, None
                                                 )
             # add child instructions to the model
